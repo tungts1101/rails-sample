007776221c316145b69601adcbfbaf42e909c60d
Tran Son Tung <tungts1101@gmail.com>
2019-03-27 02:47:46 +0000

    add image upload & image comments functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizier.permit :sign_up, keys: [:name,:age]
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name,:age]
     
-    devise_parameter_sanitizier.permit :account_update, keys: [:name, :age]
+    devise_parameter_sanitizer.permit :account_update, keys: [:name, :age]
   end
 end
